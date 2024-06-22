1c9c122e05c7b9932b72d652f365e3ad6064e196
Prajwal <pb601027@gmail.com>
2024-06-22 18:15:14 -0400

    started implementing a layer class

--- neural_net.py
+++ neural_net.py
@@ -1,2 +1,21 @@
-#test
-# everything connected? 
+
+
+#improting libraries
+import numpy as np 
+
+class Layer: 
+    
+    def __init__(self):
+        self.input = None
+        self.output = None
+        
+    def forward(self,in_layer, out_layer):
+        self.bias = np.rand.randn(-5, 5)
+        self.input = in_layer
+        self.weights = np.array([np.randon.rand(len(out_layer)) for neuron in self.input]) #create a matrix and convert it into a numpy object 
+        
+        if self.weights.shape[1]==self.input.shape[0]: # number of columns in the matrix matches the number of rows in the input layer
+            out_layer = np.dot(self.weights, in_layer)       
+        
+        
+        return out_layer
