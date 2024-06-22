f4985cb8e8fb21ed7a718059c22f2548dc9ad621
Prajwal <pb601027@gmail.com>
2024-06-22 18:48:11 -0400

    created activation function, neuron class for neuron objects

--- neural_net.py
+++ neural_net.py
@@ -3,20 +3,39 @@
 #improting libraries
 import numpy as np 
 
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def activation_function(layer):
+    """
+    input: layer - vector of values created after forward propogation
+    output: 'new' layer - vector of values subject to an activation function
+    """
+    return sigmoid(layer) # in theory this /should/ return a numpy object if we only pass numpy objects through it
+
+
+class Neuron: 
+    def __init__(self, weight):
+        self.weight = np.random.randn() 
+
 class Layer: 
     
     def __init__(self):
-        self.input = None
-        self.output = None
-        
-    def forward(self,in_layer, out_layer):
-        self.bias = np.rand.randn(-5, 5)
-        self.input = in_layer
+        self.input = [Neuron.weight for _ in range(3)] #creates a list of neurons that act like a layer 
+        return self.input
+    
+    
+    def forward(self, in_layer, out_layer):
+        self.bias = np.rand.randint(-5, 5)
+        self.input = __init__()
         self.weights = np.array([np.randon.rand(len(out_layer)) for neuron in self.input]) #create a matrix and convert it into a numpy object 
         
         if self.weights.shape[1]==self.input.shape[0]: # number of columns in the matrix matches the number of rows in the input layer
             out_layer = np.dot(self.weights, in_layer)       
         
+        #apply activation function to the output layer and then add the bias 
+        out_layer = activation_function(out_layer) + self.bias 
         
         return out_layer 
     
