bd26bd4a0759138c426b4f1874cb0e22062e1595
billert <owenbcai@gmail.com>
2024-06-22 16:55:28 -0500

    set up shell of neuron layer class

--- neural_net.py
+++ neural_net.py
@@ -1,2 +1,9 @@
 #test
 # everything connected? 
+class NeuronLayer:
+    def __init__(self, input_size, output_size):
+        in_size = input_size
+        out_size = output_size
+    def input(self, index, value):
+        # index = 0-indexed neuron location within layer; value = number to pass in
+    print("placeholder")
