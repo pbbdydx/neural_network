0e93b236a0decb59000ca92ae062920c865f68b4
Prajwal <pb601027@gmail.com>
2024-06-23 00:28:24 -0400

    small tweaks, made code blocks and comments more readable. no change in code

--- neural_net.py
+++ neural_net.py
@@ -1,7 +1,7 @@
 #improting libraries
 import numpy as np 
 
-def calc_error(y_true, y_pred): # Produces the root means squared error
+def calc_error(y_true, y_pred): ### Produces the root means squared error
     if len(y_true) != len(y_pred):
         raise ValueError("The length of y_true and y_pred must be equal")
     total = 0
@@ -19,14 +19,14 @@ def create_layers(vector: list[int]):
     last_layer_count = vector[-1]
     middle_layers= vector[1:-1] 
     layers=[]
-    # Adding each layer to layers
+    ### Adding each layer to layers
     layers.append([Neuron(vector[1]) for num in range(first_layer_count)])
     neuron_index = 2
     for layer_count in middle_layers: 
         layers.append([Neuron(vector[neuron_index]) for num in range(layer_count)])
         neuron_index += 1
-    layers.append([Neuron(vector[-1]) for num in range(last_layer_count)]) # Placeholder
-    # (We will need a separate type of output neuron that has no weights)
+    layers.append([Neuron(vector[-1]) for num in range(last_layer_count)]) ### Placeholder
+    ### (We will need a separate type of output neuron that has no weights)
     return layers 
 
 def sigmoid(x):
@@ -37,37 +37,36 @@ def activation_function(layer):
     input: layer - vector of values created after forward propogation
     output: 'new' layer - vector of values subject to an activation function
     """
-    return sigmoid(layer) # in theory this /should/ return a numpy object if we only pass numpy objects through it
+    return sigmoid(layer) ### in theory this /should/ return a numpy object if we only pass numpy objects through it
 
 
 class Neuron: 
-    def __init__(self, num_weights): # num_weights reflects num of outputs
+    def __init__(self, num_weights): ### num_weights reflects num of outputs
         self.bias = np.random.randn()
         self.weights = [np.random.random() for i in range(num_weights)]
 
 class Layer: 
     
-    def __init__(self, in_list):
-        self.input = in_list #creates a list of neurons that act like a layer 
+    def __init__(self, in_list): ### Creates a list of neurons that act like a layer 
+        self.input = in_list 
         
-        
-    def __iter__(self):
+    def __iter__(self): ### Allows the list to be iterated over which helps with the next step
         return iter(self.input)
     
     def forward(self,next_layer):
+        ### Create a matrix for dot product
         matrix_of_weights = []
         
         print(self.input)
         for neuron in self:
-             
             matrix_of_weights.append(neuron.weights)
         
-        # turn matrix of weights and the next layer into a numpy array for easy computation 
+        ### turn matrix of weights and the next layer into a numpy array for easy computation 
         # matrix_of_weights = np.array(matrix_of_weights)
         # print(matrix_of_weights)
         # next_layer = np.array(next_layer)
         
-        #chatgpt suggestion ----- will try some more testing with this tomorrow 
+        ### Chatgpt suggestion ----- will try some more testing with this tomorrow 
         
         # for neuron in self: 
         #     print(neuron.weights)
@@ -82,13 +81,13 @@ class Layer:
         print("Next layer inputs (transposed):")
         print(next_layer_inputs)
         
-        #doing the forward propogating
-        if matrix_of_weights.shape[1]==next_layer_inputs.shape[0]: # number of columns in the matrix matches the number of rows in the input layer
+        ### Doing the forward propogating
+        if matrix_of_weights.shape[1]==next_layer_inputs.shape[0]: ### Number of columns in the matrix must match the number of rows in the input layer
             out_layer = np.dot(matrix_of_weights, in_layer)
         else: raise ValueError(f"The dimensions of the matrix and next layer do not allow for a compatible matrix multiplication .\nShape of matrix: {matrix_of_weights.shape}\nShape of next layer: {next_layer_inputs.shape}. ")
         
-        #apply activation function to the output layer and then add the bias 
-        out_layer = activation_function(out_layer) + self.bias #have to change this too. instead of adding a constant add the vector of biases
+        ### Apply activation function to the output layer and then add the bias 
+        out_layer = activation_function(out_layer) + self.bias ### Have to change this too. instead of adding a constant add the vector of biases
         
         
         return out_layer 
